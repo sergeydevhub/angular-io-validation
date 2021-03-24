3beec0fc7115f7c1ced7f7e3e9eea509abb748d7
sergeydevhub <work.sergey.dev@gmail.com>
2021-03-25 00:12:02 +0200

    ref: replace class-validator user entity with io-ts

--- src/app/core/entities/user.entity.ts
+++ src/app/core/entities/user.entity.ts
@@ -1,7 +1,11 @@
-export interface User {
-  name: string;
-  surname: string;
-  email: string;
-  age: number;
-  gender: string;
-}
+import * as t from 'io-ts';
+
+export const UserEntity = t.type({
+  name: t.string,
+  nickname: t.string,
+  age: t.number,
+  email: t.string,
+  gender: t.string
+});
+
+export type TUserEntity = t.TypeOf<typeof UserEntity>
