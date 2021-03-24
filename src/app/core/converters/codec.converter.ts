c90cb021690c099c7e026475fc6a8e67d2a64c7a
sergeydevhub <work.sergey.dev@gmail.com>
2021-03-25 00:16:06 +0200

    ref: codec internals

--- src/app/core/converters/codec.converter.ts
+++ src/app/core/converters/codec.converter.ts
@@ -1,31 +1,27 @@
 import * as t from 'io-ts/lib/index';
 import { pipe } from 'fp-ts/lib/pipeable';
 import { fold, left } from 'fp-ts/lib/Either';
-import { PathReporter } from "io-ts/lib/PathReporter";
+import { PathReporter } from 'io-ts/lib/PathReporter';
 import { throwError, of, Observable } from 'rxjs';
 
 export class DecodeError extends Error {}
 
-type Result<C extends t.Mixed> = (json: unknown) => Observable<t.TypeOf<C>>;
-type Decode = <Codec extends t.Mixed>(codec: Codec) => Result<Codec>
+type Result<Codec extends t.Mixed> = (json: unknown) => Observable<t.TypeOf<Codec>>;
+type Decode = <Codec extends t.Mixed>(codec: Codec) => Result<Codec>;
 
 export const decode: Decode = <Codec extends t.Mixed>(
   codec: Codec
-): Result<Codec> => {
-  return json => {
-    return pipe(
-      codec.decode(json),
-      fold(
-        error =>
-          throwError(
-            new DecodeError(
-              PathReporter.report(
-                left(error)
-              ).join('\n')
-            )
-          ),
-          data => of(data)
-      )
-    )
-  }
-};
+): Result<Codec> => json => pipe(
+  codec.decode(json),
+  fold(
+    error =>
+      throwError(
+        new DecodeError(
+          PathReporter.report(
+            left(error)
+          ).join('\n')
+        )
+      ),
+      data => of(data)
+  )
+);
